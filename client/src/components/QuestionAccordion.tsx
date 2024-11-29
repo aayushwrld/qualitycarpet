22c0f7365b1feb8c0e86439da1254154bbd02896
aayushwrld <aayush.arora@kalvium.community>
2024-11-30 00:31:25 +0530

    Edits

---
+++ client/src/components/QuestionAccordion.tsx
@@ -0,0 +1,120 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  Flex,
+  useColorModeValue,
+  Text,
+  Container,
+  AccordionIcon,
+} from "@chakra-ui/react";
+
+export default function QuestionAccordion() {
+  return (
+    <Flex
+      margin={"1rem"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("white.50", "white.800")}
+    >
+      <Container margin={0}>
+        <Accordion allowMultiple width="100%" borderColor={"black"}>
+          {/* Question 1 */}
+          <AccordionItem borderRadius="10px">
+            <AccordionButton
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              p={4}
+            >
+              <Text fontSize="md">Why choose HS Carpets & Flooring?</Text>
+              <AccordionIcon/>
+
+            </AccordionButton>
+            <AccordionPanel pb={4}>
+              <Text color="black.600">
+                At HS Carpets & Flooring, we bring years of expertise and a
+                local touch to provide top-notch flooring solutions. Our
+                tailored services, affordable pricing, and dedication to quality
+                make us the go-to choice for flooring services in Essex.
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+
+          {/* Question 2 */}
+          <AccordionItem>
+            <AccordionButton
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              p={4}
+            >
+              <Text fontSize="md">What flooring services do we offer?</Text>
+              <AccordionIcon/>
+
+            </AccordionButton>
+            <AccordionPanel pb={4}>
+              <Text color="black.600">
+                We offer a variety of flooring services, including:
+              </Text>
+              <ul>
+                <li>Carpet installation</li>
+                <li>Vinyl flooring</li>
+                <li>Laminate flooring</li>
+                <li>Wood flooring</li>
+                <li>LVT (Luxury Vinyl Tiles)</li>
+                <li>Artificial grass laying</li>
+                <li>Stair runner installation</li>
+              </ul>
+              <Text color="black.600">
+                Our services come with a warranty and are backed by public
+                liability insurance, ensuring reliability and quality in every
+                project.
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+
+          {/* Question 3 */}
+          <AccordionItem borderBottomRadius="10px">
+            <AccordionButton
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              p={4}
+            >
+              <Text fontSize="md">What makes us stand out?</Text>
+            <AccordionIcon/>
+            </AccordionButton>
+            <AccordionPanel pb={4}>
+              <Text color="black.600">
+                As a family-run business in Essex, we provide local expertise
+                and customer-centric solutions:
+              </Text>
+              <ul>
+                <li>
+                  We stay updated on the latest flooring trends and styles.
+                </li>
+                <li>
+                  Our team is highly trained and uses state-of-the-art tools.
+                </li>
+                <li>
+                  We offer customized flooring options to match your style and
+                  functional needs.
+                </li>
+                <li>
+                  Our pricing is competitive without compromising on quality.
+                </li>
+              </ul>
+              <Text color="black.600">
+                When you choose us, you get the perfect blend of craftsmanship
+                and customer service.
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+          
+        </Accordion>
+      </Container>
+    </Flex>
+  );
+}
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,21 @@
+import QuestionAccordion from "../components/QuestionAccordion.tsx";
+import CaptionCarousel from "../components/Carousel.tsx";
+
 export default function Home() {
   return (
     <>
-      <button>Click me</button>
+      <CaptionCarousel />
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti autem voluptate beatae nisi architecto quae impedit est, enim, dignissimos consequuntur ad cum atque provident aliquam error, nemo deserunt! Eius, sit.
+      Atque distinctio ipsa, neque asperiores mollitia architecto quidem nesciunt alias dignissimos obcaecati, sint beatae. Expedita consequatur minima molestiae, perferendis a, assumenda exercitationem repellat, modi optio id doloremque aliquid quidem beatae?
+      Rem sunt facilis exercitationem numquam est! Rerum, nemo voluptatem incidunt quis sequi saepe quae fuga repudiandae cum odio, totam dolorem aspernatur inventore error tempore perspiciatis vel sint facere, sed necessitatibus.
+      Molestiae consectetur assumenda, tempore voluptatibus natus culpa magni excepturi. Ipsum laborum nam iusto dolor voluptatem iure, dolore nesciunt eveniet hic. Quo autem quam commodi nostrum labore, quos debitis porro deserunt?
+      Culpa, doloremque quas quos sapiente, laudantium dolores mollitia repellat tempore, molestias impedit nulla consequatur nemo earum a maiores quisquam quasi? Corrupti ipsum esse exercitationem adipisci iste similique tempora accusantium aliquid!
+      Maiores expedita provident alias velit nesciunt quibusdam autem voluptatibus fuga quam perferendis reiciendis ratione voluptate labore quo praesentium saepe nam adipisci, quasi quidem temporibus, a, reprehenderit repudiandae vero quas! Rem.
+      Minima tempora placeat aspernatur vitae corrupti mollitia magnam explicabo ratione earum totam, eveniet nam veniam laudantium adipisci, exercitationem libero aperiam expedita iusto minus odio. Itaque recusandae atque velit doloribus natus!
+      Vitae ab voluptate ipsam voluptatem deserunt sed est. Quas provident consequuntur deserunt et omnis. Expedita quis culpa nisi illum, officia molestias quidem ipsa veritatis et minima sint quasi, consectetur incidunt.
+      Labore aperiam cupiditate voluptatum nemo veritatis ipsam et aut quaerat reiciendis dolores blanditiis, sunt suscipit alias architecto asperiores nobis sit quis. Quam, dolorem nesciunt. Architecto soluta magni corrupti nulla obcaecati.
+      Et maxime culpa, suscipit ex voluptatibus ab iure deleniti eligendi iste saepe, necessitatibus facere rem reiciendis nam reprehenderit accusantium? Quis sapiente obcaecati non accusamus saepe maiores quod qui libero consequatur.    
+      <QuestionAccordion />
     </>
   )
 }
