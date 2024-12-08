d5e6e94d74b4e111aff25a56388a1081aac1ff46
Rikhil <rikhiltaneja@gmail.com>
2024-12-08 19:30:47 +0530

    basic setup completed

---
+++ client/src/components/Rater.tsx
@@ -0,0 +1,26 @@
+import { Box, Button } from "@chakra-ui/react";
+
+export default function Rater() {
+  return (
+    <Box
+      overflow={"hidden"}
+      border={"0.8px solid black"}
+      borderRight={"0"}
+      borderRadius={"7px"}
+      display={"flex"}
+    >
+      {Array.from({ length: 10 }, (_, i) => (
+        <Button
+          borderRight={"1px solid black"}
+          flex={1}
+          backgroundColor={"transparent"}
+          colorScheme="gray"
+          borderRadius={0}
+          key={i}
+        >
+          {i}
+        </Button>
+      ))}
+    </Box>
+  );
+}
---
+++ client/src/components/TwoButtons.tsx
@@ -0,0 +1,48 @@
+import { Button, ButtonGroup, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+
+export default function TwoButtons({ exportData }: any) {
+  const [isActive, setIsActive] = useState(true);
+
+  const clickYes = () => {
+    setIsActive(true);
+    if (!isActive) {
+      exportData(true);
+    }
+  };
+  const clickNo = () => {
+    setIsActive(false);
+    if (isActive) {
+      exportData(false);
+    }
+  };
+
+  return (
+    <Flex
+      width={"100%"}
+      padding={"1vh"}
+      backgroundColor={"#edebeb"}
+      borderRadius={"10px"}
+      justifyContent={"center"}
+    >
+      <ButtonGroup width={"100%"}>
+        <Button
+          onClick={clickYes}
+          width={"50%"}
+          colorScheme="blue"
+          backgroundColor={isActive ? "" : "grey"}
+        >
+          Yes
+        </Button>
+        <Button
+          onClick={clickNo}
+          width={"50%"}
+          colorScheme="blue"
+          backgroundColor={!isActive ? "" : "grey"}
+        >
+          No
+        </Button>
+      </ButtonGroup>
+    </Flex>
+  );
+}
