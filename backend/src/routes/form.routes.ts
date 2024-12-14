e44ea52c814b9ec90c141cd56a77a34fbf7ff7bc
Rikhil <rikhiltaneja@gmail.com>
2024-12-14 12:10:35 +0530

    form completed

--- backend/src/routes/form.routes.ts
+++ backend/src/routes/form.routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import wrapAsync from "../utils/wrapAsync";
 import { newEntry } from "../controllers/form.controller";
+import { errorHandler } from "../middlewares/ErrorMiddleware";
 
 const formControl = express.Router();
-formControl.use(express.json())
+formControl.use(express.json());
 
-formControl.post("/new",newEntry)
+formControl.post("/new", newEntry);
+formControl.use(errorHandler);
 
-export default formControl
+export default formControl;
